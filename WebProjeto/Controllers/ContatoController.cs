d268bbaf9e0616c66bd1752048679ea56a250e33
Anderson Aires <98119471+andersonaires-cc@users.noreply.github.com>
2022-05-20 10:29:27 -0300

    Aplicação ASP.NET MVC

---
+++ WebProjeto/Controllers/ContatoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebProjeto.Controllers
+{
+    public class ContatoController : Controller
+    {
+        // Método principal da pagina Index da controller
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebProjeto/Controllers/NotasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebProjeto.Controllers
+{
+    public class NotasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
